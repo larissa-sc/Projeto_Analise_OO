7f897f66f8e6a71ce43621ef0e71bed08482223b
Larissa Cavalcante <112430561+larissa-sc@users.noreply.github.com>
2024-07-08 12:51:49 -0300

    upload pastas
    iniciação do projeto

---
+++ src/Produto.java
@@ -0,0 +1,46 @@
+public class Produto {
+    private String codDeBarras;
+    private String nome;
+    private String marca;
+    private int quantidade;
+
+    public Produto(String codDeBarras, String marca, String nome, int quantidade) {
+        this.codDeBarras = codDeBarras;
+        this.marca = marca;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public String getCodDeBarras() {
+        return codDeBarras;
+    }
+
+    public void setCodDeBarras(String codDeBarras) {
+        this.codDeBarras = codDeBarras;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
