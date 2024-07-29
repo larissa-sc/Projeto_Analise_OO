e568fab740f1f8fa7a43af3441a46f05041001b1
Larissa Cavalcante <112430561+larissa-sc@users.noreply.github.com>
2024-07-29 15:40:39 -0300

    Update ControladorProduto.java

--- src/br/com/ifpe/supermercado/negocio/ControladorProduto.java
+++ src/br/com/ifpe/supermercado/negocio/ControladorProduto.java
@@ -12,7 +12,52 @@ public class ControladorProduto {
 		return produtoDAO.ler(filter);
 	}
 
-    public void criarProduto(String codigoDeBarras){
+    public void criarProduto(String codigoDeBarras, String nome, String marca, int quantidade){
+	    Produto produto = procurarProduto(codigoDeBarras);
 
+	    if (produto != null){
+		    throw new NoSuchElementException("O produto com código de barras:" + codigoDeBarras + "já está cadastrado.");
+	    }
+	    else {
+		    produto = new Produto();
+		    produto.setCodigoDeBarras(codigoDeBarras);
+		    produto.setNome(nome);
+		    produto.setMarca(marca);
+		    produto.setQuantidade(quantidade);
+	    }
+
+	    produtoDAO.inserir(produto);
+    }
+	
+    public Produto lerProduto(String codigoDeBarras){
+	    Produto produto = procurarProduto(codigoDeBarras);
+
+	    if(produto == null){
+		    throw new NoSuchElementException("O código de barras" + codigoDeBarras + "não foi encontrado.");
+	    }
+	    return produto;
+    }
+
+    public void atualizarProduto(String codigoDeBarras, int quantidade){
+	    Produto produto = procurarProduto(codigoDeBarras);
+
+	    if (produto == null){
+		    throw new NoSuchElementException("O código de barras" + codigoDeBarras + "não foi encontrado.");)
+	    }
+	    else{
+		    Produto copia = produto;
+		    copia.setQuantidade(quantidade);
+	    }
+	    produtoDAO.atualizar(produtoDAO.listar().indexOF(produto), copia);
+    }
+
+    public void deletarProduto(String codigoDeBarras){
+	    Produto produto = procurarProduto(codigoDeBarras);
+	    if (produto == null){
+		    throw new NoSuchElementException("O código de barras" + codigoDeBarras + "não foi encontrado.");)
+	    }
+	    else{
+		    produtoDAO.deletar(produto);
+	    }
     }
 }
